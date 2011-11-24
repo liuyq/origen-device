24f84773de761a8ddac83c37ffd6ff4cbeabd878
Amit Pundir <amit.pundir@linaro.org>
2011-11-24 17:01:29 +0530

    ICS Origen added touchscreen configuration file
    Change-Id: I2782f24ec967887a1bcaba335425b2c924ca099c
    Author: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- device.mk
+++ device.mk
@@ -21,7 +21,8 @@ PRODUCT_COPY_FILES += \
         device/linaro/origen/init.origen.rc:root/init.origen.rc \
         device/linaro/origen/ueventd.origen.rc:root/ueventd.origen.rc \
         device/linaro/origen/initlogo.rle:root/initlogo.rle \
-        device/linaro/origen/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+        device/linaro/origen/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+        device/linaro/origen/unidisplay_ts.idc:system/usr/idc/unidisplay_ts.idc
 
 PRODUCT_PROPERTY_OVERRIDES := \
         hwui.render_dirty_regions=false
