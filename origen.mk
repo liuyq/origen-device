074b172d80e9b6f813fbac9b153cd6fea7f7942c
Botao Sun <botao.sun@linaro.org>
2011-12-04 23:36:41 -0500

    origen.mk: Change comment about the board name.
    Change-Id: I9e77c5f99563336c1749d823a951d675b37c599d
    Signed-off-by: Botao Sun <botao.sun@linaro.org>

--- origen.mk
+++ origen.mk
@@ -1,4 +1,4 @@
-# The smdkv310
+# The Samsung Origen low cost board
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/linaro/common/common.mk)
 $(call inherit-product, device/linaro/origen/device.mk)
