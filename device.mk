543531006ab5ca111568a9b5dd633ea851e7d4f2
Vishal Bhoj <vishal.bhoj@linaro.org>
2012-04-27 07:29:57 +0000

    device.mk: Enable cameraHAL in the build
    Change-Id: I4111c6dcfe35f74d57d1e9aba7a14ee257831d79
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- device.mk
+++ device.mk
@@ -48,7 +48,8 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_PACKAGES += \
         librs_jni \
-        com.android.future.usb.accessory
+        com.android.future.usb.accessory \
+	camera.origen
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
